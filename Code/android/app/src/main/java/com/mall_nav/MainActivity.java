76d9833493fd77cb9aa0b5196af4db0f572d0b10
Khodani <khodanitshisimba@gmail.com>
2019-05-23 16:26:21 +0200

    Firebase Integration Capability
    Change to Android App for Firabase DB Connection

---
+++ Code/android/app/src/main/java/com/mall_nav/MainActivity.java
@@ -0,0 +1,46 @@
+package com.mall_nav;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Mall_Nav";
+    }
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    myRef.setValue("Hello, World!");
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+
+
+}
